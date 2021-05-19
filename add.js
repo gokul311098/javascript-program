ccb35dfdf26932a3e3e66ca639d3ed38aea278d7
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-19 17:29:20 +0530

    create add.js

---
+++ add.js
@@ -0,0 +1,16 @@
+const num1 = 5;
+const num2 = 3;
+
+const sum = num1 + num2;
+
+console.log('The value of sum is ' + sum);
+
+
+//Get input value from user
+const a = parseInt(prompt('Enter the value of a'));
+const b = parseInt(prompt('Enter the value of b'));
+
+
+const value = a + b;
+
+console.log(`The value of ${a} and ${b} is ${value}`);
