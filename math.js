7be256774c456f0af32e7de9b01b0986be338500
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-19 18:00:26 +0530

    create math.js

---
+++ math.js
@@ -0,0 +1,11 @@
+var value = Math.sqrt(100);
+console.log(value);
+
+var value = Math.sqrt(1);
+console.log(value);
+
+var value = Math.sqrt(-2);
+console.log(value);//NaN
+
+var value = Math.pow(2,2);
+console.log(value);
