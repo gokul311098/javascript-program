de3c45502ad905b201f2e046693463f93ebc6a2b
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-20 10:40:13 +0530

    create areaoftriangle.js

---
+++ Areaoftriangle.js
@@ -0,0 +1,18 @@
+const base = prompt('Enter the base value of triangle');
+const height = prompt('Enter the height value of triangle');
+
+const value = (base * height)/2;
+console.log(`The area of triangle is ${value}`);
+
+
+//Known three sides
+const side1 = parseInt(prompt('Enter the value of side1'));
+const side2 = parseInt(prompt('Enter the value of side2'));
+const side3 = parseInt(prompt('Enter the value of side3'));
+
+//find semi-perimeter
+const s = (side1 + side2 + side3)/2;
+
+//find area of triangle
+const area = Math.sqrt(s * (s-side1) * (s-side2) * (s-side3));
+console.log(`The area of triangle is ${area}`);
