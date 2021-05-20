d52e6ac2452217fc3cb3818ef545289f84342eb7
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-20 17:04:05 +0530

    create checknumber.js

---
+++ checknumber.js
@@ -0,0 +1,27 @@
+var number = prompt('Enter the value of number');
+
+if(number > 0) {
+    console.log('The number is positive');
+}
+else if(number == 0) {
+    console.log('The number is zero');
+}
+else {
+    console.log('The number is negative');
+}
+
+
+//using nested if...else
+var num = prompt('Enter the value of num');
+
+if(num >= 0) {
+    if(num == 0) {
+        console.log('The number is zero');
+    }
+    else {
+        console.log('The number is positive');
+    }
+}
+else {
+    console.log('The number is negative');
+}
