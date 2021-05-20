edc47e09ccd68244bd961d17fec0a23608ad270f
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-20 16:48:36 +0530

    create randomnum.js

---
+++ randomnum.js
@@ -0,0 +1,14 @@
+var min = parseInt(prompt('Enter the value of min'));
+var max = parseInt(prompt('Enter the value of max'));
+
+var value = Math.random() * (max - min + 1) + min;
+
+console.log(`The random value between ${min} and ${max} is ${value}`);
+
+
+//using function
+function random() {
+    return Math.floor(Math.random() * (10 - 1)) + 1;
+}
+
+console.log(random());
