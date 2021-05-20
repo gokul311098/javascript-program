f71d45f4b2a4dd0a928da1bfe0879cc21a660c12
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-20 17:48:07 +0530

    create evenodd.js

---
+++ evenodd.js
@@ -0,0 +1,16 @@
+var number = prompt('Enter the value of number');
+
+if(number % 2 == 0) {
+    console.log('The given number is even');
+}
+else {
+    console.log('The given number is odd')
+}
+
+
+//using ternary operator
+var num = prompt('Enter the value of num');
+
+var result = (num % 2 == 0)? 'even' : 'odd';
+
+console.log(`The given number is ${result}`);
