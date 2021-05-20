48f74be3a965c5925d483d81a21cf78043b24d79
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-20 12:51:26 +0530

    create metres.js

---
+++ metres.js
@@ -0,0 +1,6 @@
+const factor = 0.621371;
+var kilometres = prompt('Enter the value of kilometres');
+
+var miles = kilometres * factor;
+
+console.log(`${kilometres} kilometres is equal to ${miles} miles`);
