6c9d624e52bb8aa4d4a46165166c7b747335a448
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-21 15:26:24 +0530

    create prime.js

---
+++ prime.js
@@ -0,0 +1,23 @@
+var number = prompt('Enter the value of number');
+var isprime = true;
+
+if(number == 1) {
+    console.log(`${number} is neither prime nor composite`);
+}
+else if(number > 1) {
+    for(let i = 2; i < number; i++) {
+        if(number % i == 0) {
+            isprime = false;
+            break;
+        }
+    }
+    if(isprime) {
+        console.log(`${number} is prime number`);
+    }
+    else {
+        console.log(`${number} is not a prime number`);
+    }
+}
+else {
+    console.log(`${number} is not a prime number`);
+}
