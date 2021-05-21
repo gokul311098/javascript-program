68a21255dc5024749c0affee05b8afea1dac462f
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-21 16:04:16 +0530

    create primeinterval.js

---
+++ primeinterval.js
@@ -0,0 +1,18 @@
+var lower = prompt('Enter the value of lowernumber');
+var higher = prompt('Enter the value of highernumber');
+
+
+console.log(`The given number between ${lower} and ${higher} are:`)
+
+for(let i = lower; i <= higher; i++) {
+    let isprime = true;
+    for(let j = 2; j < i; j++) {
+        if(i % j == 0) {
+            isprime = false;
+            break;
+        }
+    }
+    if(isprime  && i > 1) {
+        console.log(i);
+    }
+}
