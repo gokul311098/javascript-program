44925078193ff29abb338cf3866b2194db226e70
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-21 17:06:46 +0530

    create multiplication.js

---
+++ multiplication.js
@@ -0,0 +1,9 @@
+var number = prompt('Enter the value of number');
+var range = prompt('Enter the value of range');
+
+console.log(`The multiplication table of ${number} is:`);
+
+for(let i = 1; i <= range; i++) {
+    let result = i * number;
+    console.log(`${i} * ${number} = ${result}`);
+}
