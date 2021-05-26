445de907f8536ac6018ce0ea1bf197c52000b57d
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-26 14:21:45 +0530

    create facrecursion.js

---
+++ facrecursion.js
@@ -0,0 +1,18 @@
+var number = prompt('Enter the value of number');
+
+if (number < 0) {
+    console.log('Enter the positive number');
+}
+else {
+    let result = factorial(number);
+    console.log(`The factorial of ${number} is ${result}`);
+}
+
+function factorial(num) {
+    if (num == 0) {
+        return 1;
+    }
+    else {
+        return num * factorial(num - 1);
+    }
+}
