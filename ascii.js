d76737fd70a6a58d33a32b801a939d2d9d0b3197
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-26 16:45:50 +0530

    create ascii.js

---
+++ ascii.js
@@ -0,0 +1,17 @@
+var string = prompt('Enter the string');
+
+result = string.charCodeAt(0);
+console.log(`The ascii value of ${string} is ${result}`);
+
+res = string.charCodeAt(string.length-1);
+console.log(`The ascii value of ${string} is ${res}`);
+
+result = string.codePointAt(0);
+console.log(`The ascii value of ${string} is ${result}`);
+
+let sentence = "Hello-World!";
+
+for (let codePoint of sentence.slice(0,7)){
+    console.log(codePoint, codePoint.codePointAt(0));
+  }
+  
