4021cf73f315c28d3ddbd043cb9946f84e646cbb
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-26 14:14:38 +0530

    create fibrecursion.js

---
+++ fibrecursion.js
@@ -0,0 +1,21 @@
+var number = prompt('Enter the value of number');
+
+if (number < 0) {
+    console.log('Enter the positive number');
+}
+else {
+    for (let i = 0; i < number; i++) {
+        console.log(fibonaci(i));
+    }
+}
+
+
+function fibonaci(num) {
+    if (num < 2) {
+        return num;
+    }
+    else {
+        return fibonaci(num - 1) + fibonaci(num - 2);
+    }
+}
+
