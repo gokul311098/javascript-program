963fdd7cb41380dc03e7aa5b8b9f7e3846ff5cb8
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-27 16:45:39 +0530

    create reversestr.js

---
+++ reversestr.js
@@ -0,0 +1,33 @@
+function reverseString(str) {
+
+    newString = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+var string = prompt('Enter the string');
+
+var value = reverseString(string);
+console.log(value);
+
+
+//using built-in function
+function reverseString(str) {
+
+    splitString = str.split("");
+
+    ReverseString = splitString.reverse();
+
+    joinString = ReverseString.join('');
+
+    return joinString;
+
+}
+
+var string = prompt('Enter the string');
+
+var value = reverseString(string);
+console.log(value);
