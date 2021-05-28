4ad5dc83812c410902418ae73bd355cd761eb461
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-28 16:36:16 +0530

    create repeatchar.js

---
+++ repeatchar.js
@@ -0,0 +1,38 @@
+function repeatChar(str, letter) {
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == letter) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+const string = prompt('Enter the string');
+
+const letterString = prompt('Enter the checking character');
+
+const value = repeatChar(string, letterString);
+
+console.log(value);
+
+
+
+//using regular expression
+function RepeatChar(str, letter) {
+
+    let reg = new RegExp(letter, 'g');
+
+    let v = str.match(reg).length;
+
+    return v;
+}
+
+const string = prompt('Enter the string');
+
+const letterString = prompt('Enter the checking character');
+
+const value = RepeatChar(string, letterString);
+
+console.log(value);
