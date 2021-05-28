6c18e6850fb9ecf9ea34f5bb01e148b1bad2ea40
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-28 16:53:53 +0530

    create strToupper.js

---
+++ strToupper.js
@@ -0,0 +1,27 @@
+function upperCase(str) {
+
+    let count = str.charAt(0).toUpperCase() + str.slice(1);
+
+    return count;
+}
+
+const string = prompt('Enter the string');
+
+const value = upperCase(string);
+
+console.log(value);
+
+
+//using regexp
+function upperCase(str) {
+
+    let count = str.replace(/^./, str[0].toUpperCase());
+
+    return count;
+}
+
+const string = prompt('Enter the string');
+
+const value = upperCase(string);
+
+console.log(value);
