d5b71addbb95e51df3f1b27959c9911a717fbed2
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-31 10:56:48 +0530

    create removeproperty.js

---
+++ removeproperty.js
@@ -0,0 +1,13 @@
+const Person = {
+    name: 'John',
+    age: 40,
+    gender: 'male',
+    hobbies: ['reading', 'coding'],
+    greet: function() {
+        return 'Hi everyone'
+    },
+}
+
+console.log(Person);
+delete Person.hobbies;
+console.log(Person);
