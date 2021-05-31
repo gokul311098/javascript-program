336e6683953782d25c26d480b5482ec4c67893e0
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-31 12:34:16 +0530

    create keyexist.js

---
+++ keyexist.js
@@ -0,0 +1,36 @@
+const person = {
+    name: 'John',
+    age: 21,
+    gender: 'male'
+}
+
+let a = prompt('Enter the key');
+
+let prop = a in person;
+
+if (prop) {
+    console.log('Key exists');
+}
+else {
+    console.log('Key does not exists');
+}
+
+
+
+//using hasOwnProperty
+const person = {
+    name: 'sam',
+    age: 23,
+    gender: 'male'
+}
+
+let a = prompt('Entee the key');
+
+let prop = person.hasOwnProperty(a);
+
+if (prop) {
+    console.log('Key exists');
+}
+else {
+    console.log('Key does not exists');
+}
