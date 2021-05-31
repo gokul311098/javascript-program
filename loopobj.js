d095aef35fa74587f2195d071c0b69e935a76cdf
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-05-31 15:53:30 +0530

    create loopobj.js

---
+++ loopobj.js
@@ -0,0 +1,25 @@
+const person = {
+    name: 'John',
+    age: 23,
+    hobbies: ['reading', 'writing']
+}
+
+const student = {
+   gender: 'male'
+}
+
+person.__proto__ = student;
+
+for (let x in person) {
+    console.log(x + '-' + person[x]);
+}
+
+/* const person1 = {
+    name:'sam',
+    age: 21,
+    gender: 'male'
+}
+
+for (let [key, value] of Object.entries(person1)) {
+    console.log(key + '-' + value);
+} */
