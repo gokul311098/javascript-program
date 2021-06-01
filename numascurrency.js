effe00c4d35eefd41e80ba24f5803657aaf57d17
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-01 14:29:13 +0530

    create numascurrency.js

---
+++ numascurrency.js
@@ -0,0 +1,13 @@
+const number = 2500;
+
+const value = '$' + number;
+console.log(value);
+
+
+//using toLocalString
+const number2 = (2500).toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD'
+});
+
+console.log(number2);
