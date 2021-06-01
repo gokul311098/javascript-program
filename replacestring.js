ad718b954db3f228c16bd4fd8a97b7ad7a95f8cd
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-01 11:02:09 +0530

    create replacestring.js

---
+++ replacestring.js
@@ -0,0 +1,18 @@
+const string = 'Mr red has a red house and red car';
+
+const regex = /red/gi;
+
+const value = string.replace(regex, 'blue');
+console.log(string);
+console.log('after changing');
+console.log(value);
+
+
+//using built-in function
+const string1 = 'Mr red has a red house and red car';
+
+const regexp = string.split('red');
+const value1 = regexp.join('blue');
+console.log(string1);
+console.log(regexp);
+console.log(value1);
