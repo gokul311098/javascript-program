2499c9cdcf9d193bc2dbe094bb715956bc848398
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-01 11:36:36 +0530

    create multiline.js

---
+++ multilinestr.js
@@ -0,0 +1,13 @@
+const string = 'This is long message\n' + 'In this tutorial\n' + 'You will learn javascript progrmming language';
+
+console.log(string);
+
+const string1 = 'This is long message\n \ In this tutorial\n \ You will learn javascript programming language';
+
+console.log(string1);
+
+const string2 = `This is long message
+ In this tutorial
+  You will learn javascript programming language`;
+
+console.log(string2);
