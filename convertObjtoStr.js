412b0a340f17ca20ac0727dae72d40c9e5db6f9d
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-02 10:15:48 +0530

    create convertObjtiStr.js

---
+++ convertObjtoStr.js
@@ -0,0 +1,20 @@
+const person = {
+    name: 'John',
+    age: 23,
+    gender: 'male'
+}
+
+const result = JSON.stringify(person);
+console.log(result);
+console.log(typeof(result));
+
+
+const person1 = {
+    name: 'sam',
+    age: 24,
+    gender: 'male'
+}
+
+const result1 = String(person1);
+console.log(result1);
+console.log(typeof(result1));
