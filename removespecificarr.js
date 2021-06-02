34c36e66720fa815b0c8143b198a933ad5f381e5
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-02 17:06:05 +0530

    removespecificarr.js

---
+++ removespecificarr.js
@@ -0,0 +1,28 @@
+function removearray(arr, n) {
+    let newArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== n) {
+            newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+}
+
+const array = removearray([1, 2, 3, 4, 5], 2);
+console.log(array);
+
+
+
+//using splice()
+function removearray1(arr1, n1) {
+    let index = arr1.indexOf(n1);
+
+    if (index > -1) {
+        arr1.splice(index, 1);
+    }
+    return arr1;
+}
+
+const array1 = removearray1([1, 2, 3, 4, 5], 1);
+console.log(array1);
