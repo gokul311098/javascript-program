39e00425a55d6b184a1396ce505675aa3b4caa1b
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-02 11:45:01 +0530

    strTobase64.js

---
+++ strTobase64.js
@@ -0,0 +1,10 @@
+const string = 'Learning Javascript Program';
+
+const result = window.btoa(string);
+
+console.log(string);
+console.log(result);
+
+const result1 = window.atob(result);
+
+console.log(result1);
