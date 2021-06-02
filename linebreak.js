919471d0d1de9eb71ec73d56110fd80e0901dda5
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-02 14:43:14 +0530

    linebreak.js

---
+++ linebreak.js
@@ -0,0 +1,21 @@
+const string = `Hi Everyone.
+Welcome to this javascript Tutorial.
+To Learn javascript Programming Language`;
+
+
+const result = string.split('\n').join('<br>');
+
+console.log(string);                
+console.log(result);
+
+
+
+//using regexp
+const string1 = `Hi Everyone.
+Welcome to this javascript Tutorial.
+To Learn javascript Programming Language`;
+
+const result1 = string.replace(/\n/g, '<br>');
+
+console.log(string1);
+console.log(result1);
