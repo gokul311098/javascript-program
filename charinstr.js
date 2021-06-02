eef8be2e64a3d7724c2cf70a462143e8ebf99108
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-02 14:34:12 +0530

    charinstr.js

---
+++ charinstr.js
@@ -0,0 +1,14 @@
+const string = 'Javascript Programming Language';
+
+const regex = string.replace(/a/g, 'A');
+console.log(regex);
+
+
+//using built-in function
+const string1 = 'Javascript Programming Language';
+
+const regex1 = string1.split('a');
+const regex2 = regex1.join('A');
+
+console.log(string1);
+console.log(regex2);
