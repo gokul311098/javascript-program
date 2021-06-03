a9c116950a52f4c0ab10b745f57363796122366d
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-03 12:10:53 +0530

    Find LCM

--- lcm.js
+++ lcm.js
@@ -1,7 +1,6 @@
 //using hcf
 var number1 = prompt('Enter the value of number1');
 var number2 = prompt('Enter the value of number2');
-
 var hcf;
 
 for (let i = 1; i <= number1 && i <= number2; i++) {
