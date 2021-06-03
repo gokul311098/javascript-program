78d0db935d7b0da83a9040251f68a84cb5676fed
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-03 12:13:41 +0530

    Find square root

--- math.js
+++ math.js
@@ -1,11 +1,11 @@
 var value = Math.sqrt(100);
 console.log(value);
 
-var value = Math.sqrt(1);
-console.log(value);
+var value1 = Math.sqrt(1);
+console.log(value1);
 
-var value = Math.sqrt(-2);
-console.log(value);//NaN
+var value2 = Math.sqrt(-2);
+console.log(value2);//NaN
 
-var value = Math.pow(2, 2);
-console.log(value);
+var value3 = Math.pow(2, 2);
+console.log(value3);
