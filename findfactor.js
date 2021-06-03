ebf064f40ef00a937557aa16507676cd078e4cd4
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-03 11:30:28 +0530

    Find factor of given number

--- findfactor.js
+++ findfactor.js
@@ -2,6 +2,7 @@ var number = prompt('Enter the value of number');
 
 console.log(`The factor of ${number} are:`);
 
+
 for (let i = 1; i <= number; i++) {
     if (number % i == 0) {
         console.log(i);
