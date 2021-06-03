42bc2b14cb8fcdebb7622915e654a4df69f1a123
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-03 11:22:39 +0530

    Check the temperature

--- degree.js
+++ degree.js
@@ -6,6 +6,7 @@ var farhen = celsius * 1.8 + 32;
 console.log(`${celsius} degree celsius is equal to ${farhen} degree farhenheit`);
 
 
+
 //find farhenheit
 var farhenheit = prompt('Enter the farhenheit value');
 
