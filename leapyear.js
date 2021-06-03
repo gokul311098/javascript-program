d90f6476850fb4643962ee96fe673b0028106b7d
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-03 12:11:21 +0530

    Find leap year

--- leapyear.js
+++ leapyear.js
@@ -13,7 +13,7 @@ function leapyear(y) {
     let Y = new Date(y, 1, 29).getDate() == 29;
 
     if (Y) {
-        return`${year1} is leap year`;
+        return `${year1} is leap year`;
     }
     else {
         return `${year1} is not a leap year`;
