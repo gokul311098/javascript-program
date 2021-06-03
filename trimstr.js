051915fb515ad7b14057b82d6b4d5e7d7f3223b3
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-03 12:52:47 +0530

    Trim a string

--- trimstr.js
+++ trimstr.js
@@ -5,8 +5,9 @@ const result = string.trim();
 console.log(result);
 
 
+//using regexp
 const string1 = '   hello world     ';
 
-const result1 = string.replace(/^\s+|\s+\s$/g,'');
+const result1 = string.replace(/^\s+|\s+\s$/g, '');
 
 console.log(result1);
