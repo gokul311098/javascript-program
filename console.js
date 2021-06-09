a4c0ed262fa35f1b7bed8c155696ffe695350831
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-09 11:34:03 +0530

    Write a console

---
+++ console.js
@@ -0,0 +1,23 @@
+let x = 'Hello';
+console.log(x);
+
+function greet() {
+    return 'Hi';
+}
+
+console.log(greet());
+
+console.log(8);
+
+console.log('Hi');
+
+
+let name = 'sam';
+console.log('Hi ' + name);
+
+let person = {
+    name: 'lilly',
+    age:21
+}
+
+console.log(person);
