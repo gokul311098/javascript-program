83e3cd04afa3be524eaab11c688938db5b85361b
Gopalakrishnagokhale <kingkrishna809@gmail.com>
2021-06-10 12:13:39 +0530

    Implement a stack

---
+++ stack.js
@@ -0,0 +1,42 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    add(elememnt) {
+        return this.items.push(elememnt);
+    }
+
+    remove() {
+        return this.items.pop();
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    clear() {
+        return this.items = [];
+    }
+}
+
+let stack = new Stack();
+console.log(stack.items);
+
+stack.add(1);
+stack.add(2);
+stack.add(3);
+stack.add(4);
+console.log(stack.items);
+
+console.log(stack.remove());
+console.log(stack.items);
+console.log(stack.peek());
+stack.add(5);
+console.log(stack.items);
+console.log(stack.size());
+console.log(stack.clear());
